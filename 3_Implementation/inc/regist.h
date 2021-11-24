1b18552cfee100b50d82136e96aa46af9d72080d
anjani321213 <77137326+anjani321213@users.noreply.github.com>
2021-11-24 18:20:50 +0530

    Update regist.h

--- 3_Implementation/inc/regist.h
+++ 3_Implementation/inc/regist.h
@@ -18,7 +18,41 @@
 #define ANS 15
 #define ACS 4
 
+void c_code(char[]);
 int check(char[]);
+
+void curser(int);
+void dbill();
+void d_mainmenu();
+void display(rec *,int,int);
+void window(int,int,int,int);
+void dis_con();
+void d_search();
+void highlight(int,int);
+
+void bill() ;
+void edit();
+void add();
+void del();
+void exit();
+
+
+void d_code();
+void d_rate();
+void d_quan();
+void d_all();
+
+
+int main()
+{
+    d_mainmenu();
+    return 0;
+}
+
+
+
+
+
 
 
 
