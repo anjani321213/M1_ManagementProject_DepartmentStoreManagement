32146b5d44f54889cd774473351cf2b6224cf012
anjani321213 <77137326+anjani321213@users.noreply.github.com>
2021-11-25 10:14:14 +0530

    Update regist.c

--- 3_Implementation/test/regist.c
+++ 3_Implementation/test/regist.c
@@ -5,36 +5,57 @@
 #include <regist.h>
 #define PROJECT_NAME    "Store Management System"
 
-/* Prototypes for all the test functions */
-void test_billing(void);
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<windows.h>
+
+#define ANS 15
+#define ACS 4
+COORD coord= {0,0}; // this is global variable
+void gotoxy(int x,int y)
+{
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+void c_code(char[]);
+int check(char[]);
+
+typedef struct
+{
+    char name[ANS],code[ACS];
+    float rate;
+    int quantity;
+} rec;
+rec item;
+
+void curser(int);
+void dbill();
+void d_mainmenu();
+void display(rec *,int,int);
+void window(int,int,int,int);
+void dis_con();
+void d_search();
+void highlight(int,int);
+
+void bill() ;
+void edit();
+void add();
+void del();
+void exit();
 
 
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
+void d_code();
+void d_rate();
+void d_quan();
+void d_all();
 
-/* Start of the application test */
+
 int main()
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_billing);
-
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
-}
-
-/* Write all the test functions */ 
-void test_billing(void) {
-  TEST_ASSERT_EQUAL(1, open(quan.dat));
-  TEST_ASSERT_EQUAL(2, open(price.dat));
-  TEST_ASSERT_EQUAL(3, open(Invoice.dat));
- 
-    /* Dummy fail*/
-  // TEST_ASSERT_EQUAL(0, file does not exist);
-  
+    d_mainmenu();
+    return 0;
 }
