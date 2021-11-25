18678a4e3e07f55c0ace0d032673565abae1706a
anjani321213 <77137326+anjani321213@users.noreply.github.com>
2021-11-25 10:10:00 +0530

    Update regist.h

--- 3_Implementation/inc/regist.h
+++ 3_Implementation/inc/regist.h
@@ -10,14 +10,46 @@
 #ifndef _regist_H
 #define _regist_H
 
-#include "stdio.h"
-#include "string.h"
-#include "stdlib.h"
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<windows.h>
 
-float amount(float rate, int quan, float dis, float vat);
-int j, quan, cash, w = 0;
-long cash_received, newt_amount;
-float rate, dis, vat, t_amount=0, amount1, float_part;
-char itemcode[5], date[20];
+#define ANS 15
+#define ACS 4
+COORD coord= {0,0}; // this is global variable
+void gotoxy(int x,int y)
+{
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+void c_code(char[]);
+int check(char[]);
 
-#endif
+
+void curser(int);
+void dbill();
+void d_mainmenu();
+void display(rec *,int,int);
+void window(int,int,int,int);
+void dis_con();
+void d_search();
+void highlight(int,int);
+
+void bill() ;
+void edit();
+void add();
+void del();
+void exit();
+
+
+void d_code();
+void d_rate();
+void d_quan();
+void d_all();
+
+
+
